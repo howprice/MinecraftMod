097dec7d276dd7e10f0697e68d47207ddeb6d2eb
howprice <howardjohnprice@gmail.com>
2016-10-31 22:43:26 +0000

    Added README.md and updated version
    As above

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,11 +5,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+@Mod(modid = ExampleMod.MODID)
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
