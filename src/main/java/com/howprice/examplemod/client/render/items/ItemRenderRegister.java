8bf7ae2c62a212c69f605986820688b25300902d
howprice <howprice@users.noreply.github.com>
2016-11-02 19:08:04 +0000

    Three items

--- src/main/java/com/howprice/examplemod/client/render/items/ItemRenderRegister.java
+++ src/main/java/com/howprice/examplemod/client/render/items/ItemRenderRegister.java
@@ -11,11 +11,24 @@ public class ItemRenderRegister
 	// this should only be called on the client (the server doesn't do any rendering)
 	public static void registerItemRenderers()
 	{
-		String itemName = ModItems.exampleItem.getUnlocalizedName().substring(5); // .substring(5) skips over "item." at the start
-		String resourceName = ExampleMod.MODID + ":" + itemName;
-		ModelResourceLocation exampleItemModelResourceLocation = new ModelResourceLocation(resourceName, "inventory");
-		
 		// n.b. ModelLoader.setCustomModelResourceLocation must be called in preInit
+		
+		// ExampleItem
+		String exampleItemName = ModItems.exampleItem.getUnlocalizedName().substring(5); // .substring(5) skips over "item." at the start
+		String exampleItemResourceName = ExampleMod.MODID + ":" + exampleItemName;
+		ModelResourceLocation exampleItemModelResourceLocation = new ModelResourceLocation(exampleItemResourceName, "inventory");
 		ModelLoader.setCustomModelResourceLocation(ModItems.exampleItem, 0, exampleItemModelResourceLocation);
+
+		// GhostItem
+		String ghostItemName = ModItems.ghostItem.getUnlocalizedName().substring(5); // .substring(5) skips over "item." at the start
+		String ghostItemResourceName = ExampleMod.MODID + ":" + ghostItemName;
+		ModelResourceLocation ghostItemModelResourceLocation = new ModelResourceLocation(ghostItemResourceName, "inventory");
+		ModelLoader.setCustomModelResourceLocation(ModItems.ghostItem, 0, ghostItemModelResourceLocation);
+
+		// OctopusItem
+		String octopusItemName = ModItems.octopusItem.getUnlocalizedName().substring(5); // .substring(5) skips over "item." at the start
+		String octopusItemResourceName = ExampleMod.MODID + ":" + octopusItemName;
+		ModelResourceLocation octopusItemModelResourceLocation = new ModelResourceLocation(octopusItemResourceName, "inventory");
+		ModelLoader.setCustomModelResourceLocation(ModItems.octopusItem, 0, octopusItemModelResourceLocation);
 	}
 }
